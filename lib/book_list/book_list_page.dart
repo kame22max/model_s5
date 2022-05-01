f1467e02d1ef0b89f5b2e057385471da71c92e44
miyoshi akihiro <vaioma222@gmail.com>
2022-05-01 14:32:50 +0900

    first commit

--- lib/book_list/book_list_page.dart
+++ lib/book_list/book_list_page.dart
@@ -22,79 +22,32 @@ class BookListPage extends StatelessWidget {
             title: const Text("本一覧"),
             centerTitle: true,
           ),
-          body: RefreshIndicator(
+          body: Consumer<BookListModel>(
+            builder: (context, model, child) {
+              return RefreshIndicator(
+                onRefresh: () async {
+                  model.fetchBookList();
+                },
+                child: Center(
+                  child: Consumer<BookListModel>(
+                    builder: (context, model, child) {
+                      final List<Book>? books = model.books;
 
-            onRefresh: () async {
-
-            },
-            child: Center(
-              child: Consumer<BookListModel>(
-                builder: (context, model, child) {
-                  final List<Book>? books = model.books;
-
-                  if (books == null) {
-                    return const CircularProgressIndicator();
-                  }
-                    final List<Widget> widgets = books
-                        .map(
-                          (book) =>
-                          Slidable(
-                            child: Card(
-                              child: ListTile(
-                                onTap: () async {
-                                  //編集画面に遷移
-                                  final String title = await Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => EditBookPage(book),
-                                    ),
-                                  );
-
-                                  var snackBar = SnackBar(
-                                    backgroundColor: Colors.green,
-                                    content: Text(
-                                      '$titleを編集しました',
-                                      textAlign: TextAlign.center,
-                                    ),
-                                  );
-                                  model.fetchBookList();
-                                  scaffoldMessengerKey.currentState
-                                      ?.showSnackBar(snackBar);
-                                  // ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                                  // model.fetchBookList();
-                                },
-                                onLongPress: () async {
-                                  //削除する。
-                                  await showConfirmDialog(
-                                    context,
-                                    book,
-                                    model,
-                                  );
-                                },
-                                title: Text(book.title),
-                                subtitle: Text(book.author),
-                                trailing: IconButton(
-                                  icon: const Icon(Icons.delete),
-                                  onPressed: () async {
-                                    await showConfirmDialog(
-                                      context,
-                                      book,
-                                      model,
-                                    );
-                                  },
-                                ),
-                              ),
-                            ),
-                            endActionPane: ActionPane(
-                              motion: const ScrollMotion(),
-                              children: [
-                                SlidableAction(
-                                  onPressed: (context) async {
+                      if (books == null) {
+                        return const CircularProgressIndicator();
+                      }
+                      final List<Widget> widgets = books
+                          .map(
+                            (book) => Slidable(
+                              child: Card(
+                                child: ListTile(
+                                  onTap: () async {
                                     //編集画面に遷移
                                     final String title = await Navigator.push(
                                       context,
                                       MaterialPageRoute(
-                                        builder: (context) => EditBookPage(book),
+                                        builder: (context) =>
+                                            EditBookPage(book),
                                       ),
                                     );
 
@@ -109,40 +62,89 @@ class BookListPage extends StatelessWidget {
                                     scaffoldMessengerKey.currentState
                                         ?.showSnackBar(snackBar);
                                     // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                                    // model.fetchBookList();
                                   },
-                                  backgroundColor: Colors.red,
-                                  foregroundColor: Colors.white,
-                                  icon: Icons.edit,
-                                  label: '編集',
-                                ),
-                                SlidableAction(
-                                  onPressed: (BuildContext context) async {
-                                    //「削除しますか？」って聞いて、「はい」だったら削除
+                                  onLongPress: () async {
+                                    //削除する。
                                     await showConfirmDialog(
                                       context,
                                       book,
                                       model,
                                     );
                                   },
-                                  backgroundColor: Colors.indigoAccent,
-                                  foregroundColor: Colors.white,
-                                  icon: Icons.delete,
-                                  label: '削除',
+                                  title: Text(book.title),
+                                  subtitle: Text(book.author),
+                                  trailing: IconButton(
+                                    icon: const Icon(Icons.delete),
+                                    onPressed: () async {
+                                      await showConfirmDialog(
+                                        context,
+                                        book,
+                                        model,
+                                      );
+                                    },
+                                  ),
                                 ),
-                              ],
+                              ),
+                              endActionPane: ActionPane(
+                                motion: const ScrollMotion(),
+                                children: [
+                                  SlidableAction(
+                                    onPressed: (context) async {
+                                      //編集画面に遷移
+                                      final String title = await Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              EditBookPage(book),
+                                        ),
+                                      );
+
+                                      var snackBar = SnackBar(
+                                        backgroundColor: Colors.green,
+                                        content: Text(
+                                          '$titleを編集しました',
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      );
+                                      model.fetchBookList();
+                                      scaffoldMessengerKey.currentState
+                                          ?.showSnackBar(snackBar);
+                                      // ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                                    },
+                                    backgroundColor: Colors.red,
+                                    foregroundColor: Colors.white,
+                                    icon: Icons.edit,
+                                    label: '編集',
+                                  ),
+                                  SlidableAction(
+                                    onPressed: (BuildContext context) async {
+                                      //「削除しますか？」って聞いて、「はい」だったら削除
+                                      await showConfirmDialog(
+                                        context,
+                                        book,
+                                        model,
+                                      );
+                                    },
+                                    backgroundColor: Colors.indigoAccent,
+                                    foregroundColor: Colors.white,
+                                    icon: Icons.delete,
+                                    label: '削除',
+                                  ),
+                                ],
+                              ),
                             ),
-                          ),
-                    )
-                        .toList();
+                          )
+                          .toList();
 
-                    return ListView(
-                      children: widgets,
-                    );
-
-
-                }
-              ),
-            ),
+                      return ListView(
+                        children: widgets,
+                      );
+                    },
+                  ),
+                ),
+              );
+            },
           ),
           floatingActionButton: Consumer<BookListModel>(
             builder: (context, model, child) {
@@ -191,31 +193,36 @@ class BookListPage extends StatelessWidget {
           title: const Text("削除の確認"),
           content: Text("『${book.title}』を削除しますか？"),
           actions: [
-            Builder(builder: (context) {
-              return TextButton(
-                child: const Text("いいえ"),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              );
-            }),
-            TextButton(
-              child: const Text("はい"),
-              onPressed: () async {
-                //modelで削除
-                await model.deleteBook(book);
-                model.fetchBookList();
-                var snackBar = SnackBar(
-                  backgroundColor: Colors.red,
-                  content: Text(
-                    '${book.title}を削除しました',
-                    textAlign: TextAlign.center,
-                  ),
+            Builder(
+              builder: (context) {
+                return TextButton(
+                  child: const Text("いいえ"),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
                 );
-                model.fetchBookList();
-                scaffoldMessengerKey.currentState
-                    ?.showSnackBar(snackBar);
-                Navigator.pop(context);
+              },
+            ),
+            Builder(
+              builder: (context) {
+                return TextButton(
+                  child: const Text("はい"),
+                  onPressed: () async {
+                    //modelで削除
+                    await model.deleteBook(book);
+                    model.fetchBookList();
+                    var snackBar = SnackBar(
+                      backgroundColor: Colors.red,
+                      content: Text(
+                        '${book.title}を削除しました',
+                        textAlign: TextAlign.center,
+                      ),
+                    );
+                    model.fetchBookList();
+                    scaffoldMessengerKey.currentState?.showSnackBar(snackBar);
+                    Navigator.pop(context);
+                  },
+                );
               },
             ),
           ],
